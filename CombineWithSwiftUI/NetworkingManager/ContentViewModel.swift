d8de17106394382ab3291607e180efbc58e13c7b
Norbert Grover <norbert298@gmail.com>
2023-12-19 11:28:29 -0800

    Finally wortking

--- CombineWithSwiftUI/NetworkingManager/ContentViewModel.swift
+++ CombineWithSwiftUI/NetworkingManager/ContentViewModel.swift
@@ -2,25 +2,18 @@ import SwiftUI
 import Combine
 
 class ContentViewModel: ObservableObject {
-    @Published var data:Data?
-    @Published var acronymObject:AcronymObject?
-    @Published var list:[String] = []
-    var cancellables:Set<AnyCancellable> = []
-    
-    @Published var listItems = ["Item 1", "Item 2", "Item 3"]
+    @Published var data: Data?
+    @Published var acronymObject: AcronymObject?
+    @Published var list: [String] = []
+    var cancellables: Set<AnyCancellable> = []
 
-    func submitAction(input: String) {
-        print("Submit button tapped with input: \(input)")
-        listItems.append(input)
-    }
-    
-    func populateListInView(search:String?) {
+    func populateListInView(search: String?) {
         var localList = [String]()
-        guard let search = search, let url = URLComponentConstants.createURLWithComponents(queryParameters: [URLQueryName.sf.rawValue:search])?.url else {
+        guard let search = search, let url = URLComponentConstants.createURLWithComponents(queryParameters: [URLQueryName.sf.rawValue: search])?.url else {
             print(ResponseError.invalidUrl.description)
             return
         }
-        
+
         let promise = NetworkingService.shared.fetchAPIResponse(url: url)
         let acronym = promise.decode(type: AcronymObject.self, decoder: JSONDecoder())
         acronym.sink { (completion) in
